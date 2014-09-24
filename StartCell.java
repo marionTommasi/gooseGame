08146099053dcdb74dcd471dbe64794d8a73319b
marionTommasi <marion.tommasi@free.fr>
2014-09-24 23:00:21 +0200

    Create StartCell.java

---
+++ StartCell.java
@@ -0,0 +1,47 @@
+package gooseGame;
+
+public class Start implements Cell {
+
+	@Override
+	public boolean canBeLeftNow() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isRetaining() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int handleMove(int diceThrow) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isBusy() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Player getPlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void welcome(Player player) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
