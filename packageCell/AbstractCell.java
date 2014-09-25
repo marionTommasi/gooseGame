25d1466145613f255481eecafeb5d9dc27c3d81c
marionTommasi <marion.tommasi@free.fr>
2014-09-25 18:52:45 +0200

    Create AbstractCell.java

---
+++ packageCell/AbstractCell.java
@@ -0,0 +1,51 @@
+package gooseGame.cell;
+
+import gooseGame.Player;
+
+public abstract class AbstractCell implements Cell {
+
+	private int index;
+	private Player player;
+
+	public AbstractCell(int i) {
+		this.index = i;
+		this.player = null;
+	}
+
+	@Override
+	public boolean canBeLeftNow() {
+		return true;
+	}
+
+	@Override
+	public boolean isRetaining() {
+		return false;
+	}
+
+	@Override
+	public int getIndex() {
+		return this.index;
+	}
+
+	@Override
+	public int handleMove(int diceThrow) {	
+		return (this.index + diceThrow);
+	}
+
+	@Override
+	public boolean isBusy() {
+		return (this.player==null);
+	}
+
+	@Override
+	public Player getPlayer() {
+		return this.player;
+	}
+
+	@Override
+	public void welcome(Player player) {
+		this.player = player;
+		player.setCell(this);
+	}
+
+}
