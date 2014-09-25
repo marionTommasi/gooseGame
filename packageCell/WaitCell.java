2449c4b4a88039a263abfcd358f44cd3091c82f3
marionTommasi <marion.tommasi@free.fr>
2014-09-25 18:38:30 +0200

    Update WaitCell.java

--- packageCell/WaitCell.java
+++ packageCell/WaitCell.java
@@ -1,46 +1,43 @@
-package gooseGame;
+package gooseGame.cell;
+
+import gooseGame.Player;
 
 /**
- * Class for the wait cell.
- * When a player jumps on a wait cell, he/she must wait a certain number of turn,
- * defined at the construction of the cell. 
- * (Except if another player jumps on the same cell and take his/her place.)
- * 
- * @author marion
- *
- */
-public class Wait extends Normal {
-
-	private int nbTurns;
-	private int timer;	
-	
-	/**
-	 * 
-	 * @param i the index of the cell
-	 * @param n the number of turn the player must wait.
-	 */
-	public Wait(int i, int n){
-		super(i);
-		this.nbTurns = n;
-		this.timer = n;
-	}
-
-	/**
-	 * Is true only if the player has wait <code>nbTurns</code>, i.e when <code>timer = -1</code>
-	 * @return true if the player can left the cell, false otherwise
-	 */
-	public boolean canBeLeftNow() {
-		this.timer--;
-		return (this.timer==-1);
-	}
-
-	/**
-	 * The timer is reset to 0 when a player jumps on this cell.
-	 * @param player 
-	 */
-	public void welcome(Player player){
-		super.welcome(player);
-		this.timer = nbTurns;
-	}
-
+* Class for the wait cell.
+* When a player jumps on a wait cell, he/she must wait a certain number of turn,
+* defined at the construction of the cell.
+* (Except if another player jumps on the same cell and take his/her place.)
+*
+* @authors Marion Tommasi - Alexandre Moevi
+*
+*/
+public class WaitCell extends AbstractCell {
+private int nbTurns;
+private int timer;	
+/**
+*
+* @param i the index of the cell
+* @param n the number of turn the player must wait.
+*/
+public WaitCell(int i, int n){
+super(i);
+this.nbTurns = n;
+this.timer = n;
+}
+/**
+* Is true only if the player has wait <code>nbTurns</code>, i.e when <code>timer = -1</code>
+* @return true if the player can left the cell, false otherwise
+*/
+public boolean canBeLeftNow() {
+this.timer--;
+return (this.timer==-1);
+}
+/**
+* The timer is reset to 0 when a player jumps on this cell.
+* @param player
+*/
+public void welcome(Player player){
+super.welcome(player);
+this.timer = nbTurns;
+}
 }
