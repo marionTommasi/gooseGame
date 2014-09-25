49ed55c8ad18c00dfca413817e79c8c69e3b0fe1
marionTommasi <marion.tommasi@free.fr>
2014-09-25 21:02:14 +0200

    Update CellStart.java

--- packageCell/CellStart.java
+++ packageCell/CellStart.java
@@ -1,14 +1,24 @@
-package jeudeloie.cells;
+package gooseGame.cell;
 
-import jeudeloie.Cell;
-import jeudeloie.Player;
+import gooseGame.Player;
 
-public class CellStart implements Cell {
+/**
+ * Class for the start cell. 
+ * There can be only one start cell on the board, and it is the first cell (index = 0). 
+ * It's the only cell where there can be more than one player.
+ * 
+ * @author Marion Tommasi - Alexandre Moevi
+ *
+ */
+public class StartCell implements Cell {
 
 	private int index = 0;
-	
+
+	/**
+	 * We don't have to know the players who are on the start cell.
+	 */
 	public Player getPlayer() throws IllegalStateException {
-		throw new IllegalStateException();		
+		throw new IllegalStateException();
 	}
 
 	public boolean canBeLeftNow() {
@@ -27,10 +37,17 @@ public class CellStart implements Cell {
 		return diceThrow;
 	}
 
+	/**
+	 * Even though there are players on the start cell, it's never busy (the
+	 * players can always return to the start cell, without swapping with the
+	 * players already there).
+	 */
 	public boolean isBusy() {
 		return false;
 	}
 
-	public void welcome(Player player) {return;}
+	public void welcome(Player player) {
+		return;
+	}
 
 }
