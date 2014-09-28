4e6d8edf4db942bb06c133a4977e13cc240b3d3e
marionTommasi <marion.tommasi@etudiant.univ-lille1.fr>
2014-09-28 22:44:32 +0200

    Update PlayerTest.java

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import gooseGame.Cell;
 import gooseGame.Player;
 import gooseGame.cell.NormalCell;
+import gooseGame.cell.StartCell;
 
 import org.junit.Test;
 
@@ -17,6 +18,7 @@ public class PlayerTest {
 
 		Player p2 = new Player();
 		assertEquals(null, p2.getCell());
+		p2.setCell(new StartCell());
 		p2.setName("Marguerite Duras");
 		assertEquals("Marguerite Duras", p2.getName());
 		
@@ -25,9 +27,12 @@ public class PlayerTest {
 		assertEquals(cell, p.getCell());
 		
 		Player p3 = new Player("Aragorn");
-		p.setCell(cell);
-		assertTrue(p3.equals(p));
-		assertFalse(p3.equals(p2));
+		p3.setCell(cell);
+		assertTrue(p3.equals(p));/* p3 and p have the same name and are on the same cell */
+		assertFalse(p3.equals(p2));/* p3 and p2 have different names and are on different cells */
+		p3.setCell(new StartCell());
+		assertFalse(p3.equals(p));/* p3 and p have the same name but are on different cells */
+		assertFalse(p3.equals(p2));/* p3 and p2 are on the same cell but have different names */
 	}
 
 }
