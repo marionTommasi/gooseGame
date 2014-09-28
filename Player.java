e7b8b2b0ee2ea8a3ea5c74309b52293329f4c6ba
marionTommasi <marion.tommasi@etudiant.univ-lille1.fr>
2014-09-28 19:55:49 +0200

    Update and rename Player to Player.java

--- Player.java
+++ Player.java
@@ -1,38 +1,66 @@
 package gooseGame;
 
-
 /**
  * Class for the players. A player and his/her goose are assimilated.
  * 
- * @author marion
+ * @author Marion Tommasi - Alexandre Moevi
  *
  */
 public class Player {
 
-	private Cell cell;
-	
+	private Cell cell;	
 	private String name;
 	
+
 	public Player(){
 		this.cell = null;
 	}
+
+	/**
+	 * 
+	 * @param name the name of the player
+	 */
+	public Player(String name){
+		this.name = name;
+		this.cell = null;
+	}
 	
+	/**
+	 * 
+	 * @return the cell where the player is.
+	 */
 	public Cell getCell(){
 		return this.cell;
 	}
 	
+	/**
+	 * 
+	 * @param cell the cell where the player arrives on
+	 */
 	public void setCell(Cell cell){
 		this.cell = cell;
 	}
 	
+	/**
+	 * 
+	 * @return the name of the player
+	 */
 	public String getName() {
 		return this.name;
 	}
 
+	/**
+	 * Changes the name of the player.
+	 * 
+	 * @param name the new name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/**
+	 * Two players are equals if they are the same player i.e are on the same cell with the same name.
+	 */
 	public boolean equals(Object o){
 		if (o == this) return true;
 		if (o == null) return false;
